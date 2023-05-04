4985f26dad97b4aad598ebeb6fda25842768e455
kobi070 <kobi070@gmail.com>
2023-05-04 11:30:20 +0300

    Legend.tsx webpack.config index.tsx packge.json update v1.0

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import app from '../examples/ReactIntro/App'
+
+import { Legend } from './components/Legend';
+
+ReactDOM.render(<App />, document.getElementById('root'))
