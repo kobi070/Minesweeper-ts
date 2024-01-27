3cbbed31761963463d78a92ec219d4cac8926aa9
kobi070 <kobi070@gmail.com>
2024-01-27 12:51:21 +0200

    Level commit #2

--- src/components/Scoreboard/Level.stories.tsx
+++ src/components/Scoreboard/Level.stories.tsx
@@ -1,22 +1,3 @@
-// import { StoryFn, Meta } from "@storybook/react";
-// import { Counter, CounterProps } from "./Counter";
-// import React from "react";
-
-// export default {
-//     title: "Scoreboard/Couter",
-//     component: Counter,
-//     tags: ['autodocs']
-// } as Meta
-
-// const Template : StoryFn<CounterProps> = (args) => <Counter {...args} />;
-
-// export const ScoreboardCounter = Template.bind({});
-
-// ScoreboardCounter.args = {
-//     children: '10',
-// }
-
-
 import React from "react";
 import { StoryFn, Meta } from "@storybook/react";
 
@@ -32,3 +13,10 @@ const Template : StoryFn<LevelProps> = (args) => <Level {...args} />;
 
 export const ScoreboardLevel = Template.bind({});
 
+ScoreboardLevel.args = {
+    children: [
+        "Begginer",
+        "Advance",
+        "Expert",
+    ]
+}
