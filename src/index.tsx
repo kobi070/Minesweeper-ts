13d501ee63383dbabea724c5ad7531a8bf3bf590
kobi070 <kobi070@gmail.com>
2024-01-28 10:22:35 +0200

    index.tsx update @4

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Top } from './components/Top';
+import { Top } from "./components/Top";
+import { Scoreboard } from "./components/Scoreboard";
 
 // import App from '../examples/ReactIntro/App'
 // import { Legend } from './components/Legend/Legend';
 
 ReactDOM.render(
-    <Top feature='Flag' firstAction='ctrl' secondAction='click'>
-        Minesweeper
-    </Top>,
-    document.getElementById('root')
+  <>
+    <Top feature="Flag" firstAction="ctrl" secondAction="click">
+      Minesweeper
+    </Top>
+    <Scoreboard
+      levels={["begginer", "advanced", "expert"]}
+      mines="0"
+      onReset={() => {}}
+      timer="010"
+    />
+  </>,
+  document.getElementById("root")
 );
