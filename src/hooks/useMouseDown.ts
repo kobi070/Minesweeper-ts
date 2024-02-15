1243e6141f37d8bb736761f7a05806e6ed6f746e
kobi070 <kobi070@gmail.com>
2024-02-15 11:03:04 +0200

    (#24 dev update) updated hooks to use useDebugValue

--- src/hooks/useMouseDown.ts
+++ src/hooks/useMouseDown.ts
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useState, useDebugValue } from "react";
 
 export type setMouseDown = () => void;
 export type setMouseUp = () => void;
+
 export const useMouseDown = (): [boolean, setMouseDown, setMouseUp] => {
   const [mouseDown, setMouseDown] = useState(false);
+
+  console.log(`mouseDown: ${mouseDown}`);
+  useDebugValue(
+    mouseDown,
+    (mouseDown) => {
+      const isoTimestamp = new Date().toISOString();
+      return `${mouseDown ? "Mouse is down" : "Mouse is up"} - ISO Time: ${isoTimestamp}`;
+    }
+  );
 
   const onMouseDown = () => setMouseDown(true);
   const onMouseUp = () => setMouseDown(false);
